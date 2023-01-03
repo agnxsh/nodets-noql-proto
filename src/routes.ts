892bbc3d4ac716f009e9f53a91a428c2bb4da049
Agnish Ghosh <iamagnix@gmail.com>
2023-01-03 18:47:54 +0530

    validator added

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
-import { Express } from "express";
-function routes(app: Express) {}
+import { Express, Request, Response } from "express";
+function routes(app: Express) {
+  app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+}
 
 export default routes;
