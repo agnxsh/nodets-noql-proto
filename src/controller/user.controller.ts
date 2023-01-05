4ab2fccae8a06d3ad88e5193591804a90e42d30f
Agnish Ghosh <iamagnix@gmail.com>
2023-01-05 18:31:41 +0530

    started with User Creation Service

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,5 +2,9 @@ import { Request, Response } from "express";
 import logger from "../utils/logger";
 export function createUserHandler(req: Request, res: Response) {
   try {
-  } catch (e) {}
+    //const user = await "will call the createUser service"
+  } catch (e) {
+    logger.error(e);
+    return res.status(409).send();
+  }
 }
