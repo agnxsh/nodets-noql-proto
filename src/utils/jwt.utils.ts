ae09dd12491fa2660392859953d637f10351cf98
Agnish Ghosh <iamagnix@gmail.com>
2023-01-14 15:08:24 +0530

    feat: access token verification util complete

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,6 +1,23 @@
 import jwt from "jsonwebtoken";
 import config from "config";
 
-function signJwt() {}
+const privateKey = config.get<string>("privateKey");
+const publicKey = config.get<string>("publicKey");
 
-function verifyJwt() {}
+export function signJwt(object: Object, options?: jwt.SignOptions | undefined) {
+  return jwt.sign(object, privateKey, {
+    ...(options && options),
+    algorithm: "RS256",
+  });
+}
+
+export function verifyJwt(token: string) {
+  try {
+    const decoded = jwt.verify(token, publicKey);
+  } catch (e: any) {
+    return {
+      valid: false,
+      expired: e.message === "jwt expired",
+    };
+  }
+}
